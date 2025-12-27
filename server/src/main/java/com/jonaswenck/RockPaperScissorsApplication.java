ebd2f4ae54cd107730557fb3933d593fb605cf89
Jonas Wenck <jonasw.goes@gmail.com>
2025-12-27 14:02:15 +0100

    #2: set up Spring Boot server

---
+++ server/src/main/java/com/jonaswenck/RockPaperScissorsApplication.java
@@ -0,0 +1,12 @@
+package com.jonaswenck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsApplication {
+    static void main(String[] args) {
+        SpringApplication.run(RockPaperScissorsApplication.class, args);
+    }
+
+}
